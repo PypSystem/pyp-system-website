9229be810e6fe80e38108002676cd2d5434d894c
Vittor de Aguiar <vittoraguiar93@gmail.com>
2024-09-11 11:21:15 -0300

    adicionando poolitica de privacidede para app do ppci

---
+++ pages/PpciPrivacyPolicy.vue
@@ -0,0 +1,128 @@
+<script setup></script>
+
+<template>
+  <section class="bg-gray-100 py-8 text-zinc-900">
+    <div class="container mx-auto px-6">
+      <h1 class="text-3xl font-bold text-center mb-6">
+        POLÍTICA DE PRIVACIDADE
+      </h1>
+      <p class="mb-4">
+        Esta Política de Privacidade se refere ao aplicativo
+        <strong>PPCI APP</strong>, desenvolvido pela Pyp System LTDA. O objetivo
+        deste documento é informar como coletamos, usamos, protegemos e
+        compartilhamos as informações dos usuários. Ao utilizar o aplicativo,
+        você concorda com as práticas descritas nesta Política de Privacidade.
+      </p>
+
+      <h2 class="text-xl font-semibold mt-6">
+        1. Informações Fornecidas pelo Usuário
+      </h2>
+      <p class="mb-4">
+        As informações fornecidas pelo usuário dentro dos recursos do aplicativo
+        são armazenadas apenas para o funcionamento do PPCI APP. Esses dados são
+        utilizados exclusivamente para gerar plantas baixas baseadas nas
+        regulamentações dos bombeiros e orçamentos de materiais, sem coletar
+        informações pessoais.
+      </p>
+
+      <h2 class="text-xl font-semibold mt-6">
+        2. Coleta Automática de Informações
+      </h2>
+      <p class="mb-4">
+        O PPCI APP coleta automaticamente informações, como estatísticas de
+        instalação e utilização, para relatórios estratégicos que ajudam a
+        melhorar a experiência do usuário. Esses dados são coletados de forma
+        anônima e não incluem informações de identificação pessoal.
+      </p>
+
+      <h2 class="text-xl font-semibold mt-6">
+        3. Coleta de Informações de Localização
+      </h2>
+      <p class="mb-4">
+        O aplicativo PPCI APP não coleta informações de localização em tempo
+        real do dispositivo. Qualquer informação de localização coletada é não
+        precisa e serve apenas para fins de análise interna.
+      </p>
+
+      <h2 class="text-xl font-semibold mt-6">
+        4. Acesso de Informações por Terceiros
+      </h2>
+      <p class="mb-4">
+        As informações coletadas podem ser compartilhadas com terceiros em
+        algumas situações específicas:
+      </p>
+      <ul class="list-disc pl-6 mb-4">
+        <li>
+          <strong>Exigido por Lei:</strong> Quando a divulgação é necessária
+          para cumprir obrigações legais, proteger nossos direitos ou a
+          segurança dos usuários, investigar fraudes ou responder a uma
+          solicitação do governo.
+        </li>
+        <li>
+          <strong>Prestadores de Serviços:</strong> Compartilhamos informações
+          com prestadores de serviços que atuam em nosso nome, sob a condição de
+          que não usem essas informações para outros fins e sigam as regras
+          desta Política de Privacidade.
+        </li>
+        <li>
+          <strong>Mudanças na Estrutura da Empresa:</strong> Em caso de fusão,
+          aquisição ou venda de ativos da Pyp System, os usuários serão
+          notificados sobre qualquer alteração na propriedade ou uso das
+          informações.
+        </li>
+        <li>
+          <strong>Bombeiros:</strong> Informações podem ser compartilhadas com
+          os bombeiros para assegurar conformidade com as regulamentações de
+          segurança e para a análise de projetos.
+        </li>
+      </ul>
+
+      <h2 class="text-xl font-semibold mt-6">
+        5. Coleta de Informações para Publicidade
+      </h2>
+      <p class="mb-4">
+        Podemos coletar informações, como o ID de identificação do dispositivo e
+        outras informações anônimas para analisar o uso do aplicativo e melhorar
+        a publicidade direcionada. Utilizamos tecnologias de criptografia para
+        proteger o anonimato dessas informações, garantindo que terceiros não
+        possam identificá-lo pessoalmente.
+      </p>
+
+      <h2 class="text-xl font-semibold mt-6">6. Direitos de Exclusão</h2>
+      <p class="mb-4">
+        Você pode interromper a coleta de informações desinstalando o aplicativo
+        PPCI APP. Basta utilizar os processos de desinstalação padrão do seu
+        dispositivo móvel.
+      </p>
+
+      <h2 class="text-xl font-semibold mt-6">7. Segurança</h2>
+      <p class="mb-4">
+        A Pyp System LTDA se preocupa com a proteção dos seus dados.
+        Implementamos medidas de segurança físicas, eletrônicas e processuais
+        para proteger as informações processadas e mantidas. No entanto, nenhum
+        sistema de segurança é infalível.
+      </p>
+
+      <h2 class="text-xl font-semibold mt-6">8. Alterações e Consentimento</h2>
+      <p class="mb-4">
+        Esta Política de Privacidade pode ser atualizada periodicamente. Todas
+        as alterações serão notificadas ao usuário.
+      </p>
+
+      <h2 class="text-xl font-semibold mt-6">9. Contato</h2>
+      <p class="mb-4">
+        Caso tenha dúvidas ou precise de mais informações, entre em contato
+        conosco pelo e-mail:
+        <a href="mailto:pyp@pypsystem.com" class="text-blue-500 underline"
+          >pyp@pypsystem.com</a
+        >.
+      </p>
+
+      <p class="mt-6 font-bold">PYP SYSTEM LTDA</p>
+      <p>
+        Travessa Lira Ernesta Basso Dutra, 420, Santo Ângelo - RS - BRASIL CEP:
+        98807-468
+      </p>
+    </div>
+  </section>
+</template>
